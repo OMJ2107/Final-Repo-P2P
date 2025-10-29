2b34361b9aed91be8f55b4d8d8a7e39bffb2f2d2
Hritik <94539105+hritik080400@users.noreply.github.com>
2025-10-29 14:52:41 +0530

    Updated by hritik

--- P2PLibray/Purchase/BALPurchase.cs
+++ P2PLibray/Purchase/BALPurchase.cs
@@ -1445,10 +1445,10 @@ namespace P2PLibray.Purchase
             Dictionary<string, string> prParam = new Dictionary<string, string>();
             prParam.Add("@Flag", "NewPRSP");
             prParam.Add("@PRCode", purchase.PRCode);
-            prParam.Add("@RequiredDate", purchase.RequiredDate.ToString("yyyy-MM-dd HH:mm:ss"));
+            prParam.Add("@RequiredDate", purchase.RequiredDate.ToString("yyyy-MM-dd"));
             prParam.Add("@StatusId", purchase.Status.ToString());
             prParam.Add("@AddedBy", purchase.AddedBy);
-            prParam.Add("@AddedDate", DateTime.Now.ToString());
+            prParam.Add("@AddedDate", DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
             prParam.Add("@PriorityId", purchase.PriorityId.ToString());
 
             await obj.ExecuteStoredProcedure("PurchaseProcedure", prParam);
